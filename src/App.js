52cd20a78e3dcd5ef0d81288d567ea367587999a
Sal Ferrarello <sal.ferrarello@webdevstudios.com>
2020-05-29 15:07:45 -0400

    Remove placeholder markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import TideList from './components/TideList.js';
 import './App.css';
 
@@ -7,23 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Tide List</h1>
         <TideList
           beginDate="20200522"
           endDate="20200721"
           station="8536110"
         />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
