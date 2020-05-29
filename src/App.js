e398695afd2daf7967ac4334628acc828af3ebf6
Sal Ferrarello <sal.ferrarello@webdevstudios.com>
2020-05-29 15:53:59 -0400

    Dynamically query today and tomorrow
    Dynamically set beginDate and endDate to today and tomorrow respectively

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import TideList from './components/TideList.js';
 import './App.css';
 
 function App() {
+  var today = new Date();
+  var tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+
+  const dateToYMD = (date) =>
+    date.getFullYear().toString() +
+    (1 === (date.getMonth()+1).toString().length ? '0' : '')+
+    (date.getMonth()+1).toString() +
+    date.getDate().toString();
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Tide List</h1>
         <TideList
-          beginDate="20200522"
-          endDate="20200721"
+          beginDate={dateToYMD(today)}
+          endDate={dateToYMD(tomorrow)}
           station="8536110"
         />
       </header>
